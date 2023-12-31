26af44c99b7ee978fd29b9e3f51abbc3987e9683
Luis <siul210704@gmail.com>
2023-12-31 13:05:33 -0400

    Update _app.tsx with meta tags and favicon

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,16 +1,30 @@
 import { type PageProps } from "$fresh/server.ts";
 export default function App({ Component }: PageProps) {
   return (
-    <html>
-      <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>cine2.0</title>
-        <link rel="stylesheet" href="/styles.css" />
-      </head>
-      <body>
-        <Component />
-      </body>
-    </html>
+    <>
+      <html lang="es">
+        <head>
+          <meta charset="utf-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <title>Cines Unidos</title>
+          <meta
+            name="description"
+            content="CinesUnidos, el cine en un click. Compra y reservación de boletos, downloads, información de películas y últimos estrenos de la cartelera cinematográfica de Venezuela."
+          />
+          <meta
+            name="keywords"
+            content="Cines Unidos, Cines Unidos Caracas, Cines Unidos Maracaibo, Cines Unidos Valencia, Cines Unidos Sambil, Cines Unidos Maturin, Cines Unidos Margarita, Cartelera de Cines Unidos,  Peliculas en cartelera,  Estrenos cines unidos, Combos Cines Unidos,  Precio Entradas Cines Unidos, Promociones Cines Unidos, Descuentos Cines Unidos, Cines Unidos Descuento estudiantil"
+          />
+          <link rel="shortcut icon" href="favicon.webp" type="image/x-icon" />
+          <link rel="stylesheet" href="/styles.css" />
+        </head>
+        <body>
+          <Component />
+        </body>
+      </html>
+    </>
   );
 }
