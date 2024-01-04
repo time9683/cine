37efff72c3ed32b4d8181de70d90fccaa04e0568
Luis <siul210704@gmail.com>
2024-01-04 08:26:08 -0400

    Refactor banner image scraping and conversion

--- scrapper/home.ts
+++ scrapper/home.ts
@@ -4,6 +4,7 @@ import * as cheerio from "https://esm.sh/cheerio@1.0.0-rc.12";
 import sharp from "npm:sharp";
 
 export async function scrapeHome($: cheerio.CheerioAPI) {
+  const imgs: string[] = [];
   const banners: string[] = [];
   // check if banners folder exist
   const bannersFolder = join(Deno.cwd(), "static", "banners");
@@ -12,22 +13,23 @@ export async function scrapeHome($: cheerio.CheerioAPI) {
   });
 
   // get banners  from a class "pelivelotopimg"
-  $(".carousel-inner  .pelivelotopimg").each((i, el) => {
+  $(".carousel-inner  .pelivelotopimg").each((_i, el) => {
     console.log($(el).attr("src"));
     const url = $(el).attr("src");
     if (!url) return;
-    const output = join(Deno.cwd(), "static", "banners", i + ".webp");
-    banners.push("/banners/" + i + ".webp");
-
-    fetch(url!)
-      .then((img) => img.arrayBuffer())
-      .then((imgBuffer) =>
-        sharp(imgBuffer).webp({ effort: 6, quality: 75 }).toFile(
-          output,
-        ).then(() => console.log("done image conversion"))
-      );
+    imgs.push(url);
   });
-  join;
+
+  const promises = imgs.map(async (url, i) => {
+    const imgBuffer = await fetch(url).then((res) => res.arrayBuffer());
+    const output = join(Deno.cwd(), "static", "banners", i + ".webp");
+    await sharp(imgBuffer).webp({ effort: 6, quality: 75 }).toFile(output);
+    console.log("done", i);
+    banners.push("/banners/" + i + ".webp");
+  });
+
+  await Promise.all(promises);
+
   // save the banners in a json file ubicate in /db/banners.json
   const dbUrl = join(Deno.cwd(), "db", "banners.json");
   await Deno.writeTextFile(dbUrl, JSON.stringify(banners), {
