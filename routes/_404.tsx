73decad2d10d0b47bd83f0dd5054ebbf75e3568d
Luis <siul210704@gmail.com>
2024-01-05 21:33:37 -0400

    Refactor 404 page layout and content

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -6,22 +6,23 @@ export default function Error404() {
       <Head>
         <title>404 - Page not found</title>
       </Head>
-      <div class="px-4 py-8 mx-auto bg-[#86efac]">
-        <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-          <img
-            class="my-6"
-            src="/logo.svg"
-            width="128"
-            height="128"
-            alt="the Fresh logo: a sliced lemon dripping with juice"
-          />
-          <h1 class="text-4xl font-bold">404 - Page not found</h1>
-          <p class="my-4">
-            The page you were looking for doesn't exist.
-          </p>
-          <a href="/" class="underline">Go back home</a>
-        </div>
-      </div>
+
+      <main class="grid place-content-center max-w-4xl mx-auto p-4 text-center gap-4">
+        <h1 class="text-9xl font-bold text-white">404</h1>
+        <img src="/this-is-fine-404.gif" class="m-auto" />
+
+        <h2 class="text-2xl font-semibold text-white">Page not found</h2>
+        <p class="text-gray-500 py-2">
+          Oops! The page you are looking for does not exist. It might have been
+          moved or deleted.
+        </p>
+        <a
+          href="/"
+          class="px-4 py-2 bg-red-700 hover:bg-red-600 hover:text-white  text-white/80 rounded text-center"
+        >
+          Return Home
+        </a>
+      </main>
     </>
   );
 }
