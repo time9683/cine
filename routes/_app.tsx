7ef9dfa9270ec081ee034af6f9b8641621292f4d
Luis <siul210704@gmail.com>
2024-01-05 20:41:08 -0400

    Add f-client-nav and Partial component to _app.tsx

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,4 +1,5 @@
 import { type PageProps } from "$fresh/server.ts";
+import { Partial } from "$fresh/runtime.ts";
 export default function App({ Component }: PageProps) {
   return (
     <>
@@ -21,8 +22,10 @@ export default function App({ Component }: PageProps) {
           <link rel="shortcut icon" href="favicon.webp" type="image/x-icon" />
           <link rel="stylesheet" href="/styles.css" />
         </head>
-        <body>
-          <Component />
+        <body f-client-nav>
+          <Partial name="body">
+            <Component />
+          </Partial>
         </body>
       </html>
     </>
