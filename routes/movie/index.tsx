4cd68030b8a1267c32587a61371f3bf05575b231
Luis <siul210704@gmail.com>
2024-01-10 19:13:28 -0400

    add seo for movie page

--- routes/movie/index.tsx
+++ routes/movie/index.tsx
@@ -4,7 +4,7 @@ export default function Movies() {
   return (
     <>
       <Head>
-        <title>Cines Unidos</title>
+        <title>Cines Unidos - Peliculas</title>
       </Head>
 
       <main class="grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-4 p-4 gap-8">
