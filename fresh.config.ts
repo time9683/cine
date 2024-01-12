ebac3b9835937e7839a9fb558ee47ab24ce47f83
Luis <siul210704@gmail.com>
2024-01-12 13:41:21 -0400

    Add fresh_seo plugin and update fresh.config.ts

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
-
+import { freshSEOPlugin } from "https://deno.land/x/fresh_seo/mod.ts";
+import  manifest  from "./fresh.gen.ts";
 export default defineConfig({
-  plugins: [tailwind()],
+  plugins: [tailwind(),freshSEOPlugin(manifest)],
 });
